325dfd7b4e881ad0307cc0dfbd07882fd6a64daa
Ramy ElGendi <57822979+ramyelgendi@users.noreply.github.com>
2021-04-08 18:32:50 +0200

    Implementing scheduler funcs

--- Project1/Core/Src/main_test.c
+++ Project1/Core/Src/main_test.c
@@ -3,18 +3,40 @@
 //
 #include "queue.h"
 static struct Queue* readyQueue;
+static struct Queue* delayedQueue;
+struct Task runningTask;
 void func1(){
     printf("Task is running! \n");
 }
 
 void QueTask(func task,int priorty){
-    Enqueue(readyQueue,priorty,0,task);
+    if( priorty >=1 && priorty<=8 ) {
+        Enqueue(readyQueue, priorty, 0, task);
+    }
+
+}
+
+void Dispatch() {
+    if(!isQueuempty(readyQueue)){
+        runningTask = Dequeue(readyQueue);
+        runningTask.task();
+    }
+}
+
+void ReRunMe(int delay) {
+    runningTask.delay=delay;
+    if(delay==0)
+        QueTask(runningTask.task, runningTask.priority);
+    else
+       Enqueue(delayedQueue,runningTask.priority,runningTask.delay,runningTask.task);
 
 }
 
 int main()
 {
     readyQueue = createQueue(1000);
+    delayedQueue = createQueue(1000);
+
     Enqueue(readyQueue, 5, 0, func1);
     Enqueue(readyQueue, 2, 0, func1);
     Enqueue(readyQueue, 1, 0, func1);
@@ -28,6 +50,7 @@ int main()
         printf("Index %d, priority: %d\n",i,temp.priority);
         temp.task();
     }
+    ReRunMe();
 //    printf("%d '\n",Dequeue(queue)->priority);
 //    printf("%d '\n",Dequeue(queue)->priority);
 //    printf("%d '\n",Dequeue(queue)->priority);
